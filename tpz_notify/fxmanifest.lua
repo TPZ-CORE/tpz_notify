5847b5f76d695af022380cfead3fa48fffb5c9e7
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-16 00:53:17 +0300

    Update fxmanifest.lua

--- tpz_notify/fxmanifest.lua
+++ tpz_notify/fxmanifest.lua
@@ -8,7 +8,7 @@ version '1.0.0'
 
 ui_page 'html/index.html'
 
-shared_scripts { 'config.lua', 'locales.lua' }
+shared_scripts { 'config.lua'  }
 client_scripts { 'client/*.lua' }
 server_scripts { 'server/*.lua' }
 
