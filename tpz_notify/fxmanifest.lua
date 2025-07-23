5dca3d4187e6026f7703eb7abe8b7ac6d3cdcdbd
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-07-23 15:01:49 +0300

    Update fxmanifest.lua

--- tpz_notify/fxmanifest.lua
+++ tpz_notify/fxmanifest.lua
@@ -4,12 +4,11 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 
 author 'Nosmakos'
 description 'TPZ-CORE Notify'
-version '1.0.0'
+version '1.0.1'
 
 ui_page 'html/index.html'
 
 shared_scripts { 'config.lua' }
 client_scripts { 'client/*.lua' }
-server_scripts { 'server/*.lua' }
 
 files { 'html/**/*' }
