006a59dec290a583af908d9270bddf05dae30ea7
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-07-27 23:43:57 +0300

    Update script.js

--- tpz_notify/html/js/script.js
+++ tpz_notify/html/js/script.js
@@ -35,10 +35,20 @@ $(function() {
 				$("#notify_message").text(item.message);
 
 				$("#notify_message").css("color", item.color);
-	
-				$(".notify").fadeIn(500);
 
-			});
+				if (item.align == 'left') {
+					
+					$(".notify").css('margin-left', '-48vw');
+
+					$("#notify_title").css('margin-left', '6vw');
+					$("#notify_message").css('margin-left', '6vw');
+					$("#notify_action_image_display").css('margin-left', '1.5vw');
+
+				}else if (item.align == 'right'){
+					$(".notify").css('margin-left', '74vw');
+				}
+
+				$(".notify").fadeIn(500);
 		
 
 		}
